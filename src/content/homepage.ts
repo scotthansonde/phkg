0a3aae80cd3700ab2425b4db790e1016a210efeb
Scott Hanson <papascott@gmail.com>
2023-03-11 11:46:31 +0100

    First draft

--- src/content/homepage.ts
+++ src/content/homepage.ts
@@ -1,17 +1,27 @@
+const desktopIcons = "dark:text-zinc-300 text-black hover:text-zinc-500 dark:hover:text-zinc-400 hidden sm:block";
+
 export const homepage = [
   {
-    title: "Blazingly Fast",
-    content: "The reason why I chose Astro is because it's blazingly fast. It's also a component-based framework, so you can easily reuse your components.",
-    icon: "charm:rocket",
+    title: "Post",
+    content: "Harburger Straße 19<br />21244 Buchholz i.d.N.<br />Deutschland",
+    icon: "bi:house",
+
   },
   {
-    title: "Easy to use",
-    content: "Need to use this template? Just clone it and start writing your content. No complicated setup, no complicated design.",
-    icon: "ph:code-bold",
+    title: "Telefon",
+    content: "+49-4181-9399291",
+    icon: "bi:telephone",
+
   },
   {
-    title: "Free and Open Source",
-    content: "Found some issues or want to contribute? Feel free to open an issue or pull request on GitHub.",
-    icon: "charm:plant-pot",
+    title: "Mail",
+    content: `<a href="mailto:info@mcdonalds-nordheide.de" class="${desktopIcons}">info@mcdonalds-nordheide.de</a>`,
+    icon: "bi:envelope",
+
+  },
+  {
+    title: "Homepage",
+    content: `<a href="https://www.mcdonalds-nordheide.de" class="${desktopIcons}">www.mcdonalds-nordheide.de</a>`,
+    icon: "bi:code-slash",
   },
 ];
